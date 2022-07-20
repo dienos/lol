25f3b053ad0561cb1ea9b284a3f1137eff9584da
jeon.th <jeon.th@kstm.co.kr>
2022-07-20 18:51:40 +0900

    Dependencies.kt 오기입 수정

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -70,7 +70,7 @@ object Config {
 
 object Plugins {
     const val application = "com.android.application"
-    const val library = "com.android.application"
+    const val library = "com.android.library"
     const val kotlin = "org.jetbrains.kotlin.android"
     const val hilt = "dagger.hilt.android.plugin"
     const val kapt = "kotlin-kapt"
